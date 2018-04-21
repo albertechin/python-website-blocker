a8be9c71b71b283b855b349b230cdd51a5eb3719
Albert Abraham <abraham.albert.albert@gmail.com>
2018-04-21 11:59:40 +0530

    Website Blocker 1.0

---
+++ website-blocker.py
@@ -0,0 +1,29 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp="hosts" #temp hosts file for testing
+#hosts_path ="/etc/hosts" #hosts file path in linux machine
+hosts_path = r"c:\windows\system32\drivers\etc\hosts" #hosts file path in windows machine
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,14) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+        with open(hosts_temp,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+    else:
+        with open(hosts_temp,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours are good for health...")
+    time.sleep(5)
